96627a8e6cc019808a0c2ae741babd1acaf2e051
Wei Xu <wavyxu@gmail.com>
2018-09-27 22:12:21 -0700

    Write recode to MongoDB

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// define a schema
 const userSchema = new Schema({
     googleId: String
 });
 
+// create a model named "users", which equals a collection MongoDB
+// with scheme "userSchema"
 mongoose.model('users', userSchema);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+// create a model class named 'User'
+const  User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -9,9 +12,19 @@ passport.use(new GoogleStrategy({
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
+        User.findOne({ googleId: profile.id})
+            .then(existingUser => {
+                if (existingUser) {
+                    // we already have a record with the given profile ID
+                    done(null, existingUser);
+                } else {
+                    // we don't have a record with the given profile ID, make a new record
+                    // create a new instance of model User
+                    new User({ googleId: profile.id})
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
     }
     )
 );
