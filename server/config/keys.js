40b61ed9a86dce6b318db050b2efd05fbed2995a
Wei Xu <wavyxu@gmail.com>
2018-09-27 20:56:05 -0700

    Connect to MongoDB

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: '999855253190-297696k78gpthkbsuevukp4o64klu9lc.apps.googleusercontent.com',
-    googleClientSecret: 'uSIsqQj_Jnkd2kUYz4GcTIwj'
+    googleClientSecret: 'uSIsqQj_Jnkd2kUYz4GcTIwj',
+    mongoURI: 'mongodb://wavy:xuwei0424@ds117623.mlab.com:17623/questionnaire',
 };
