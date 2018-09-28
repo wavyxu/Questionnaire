dd235f020ca40d4f46737c01dee24b7711f0fb97
Wei Xu <wavyxu@gmail.com>
2018-09-28 11:36:32 -0700

    Encoding Users
    Encoding Users
    Deserialize User
    Enabling Cookies
    Testing Authentication

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,6 +6,17 @@ const keys = require('../config/keys');
 // create a model class named 'User'
 const  User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+});
+
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
