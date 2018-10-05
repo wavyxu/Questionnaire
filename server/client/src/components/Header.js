a93bbd766709047cec204224a35359b8f2a9b904
Wei Xu <wavyxu@gmail.com>
2018-10-04 17:28:25 -0700

    Add stripe env variables

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from "react-redux";
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -20,16 +20,15 @@ class Header extends Component {
         return (
             <nav>
                  <div className="nav-wrapper">
-                     {/*<Link*/}
-                         {/*to={this.props.user ? '/surveys' : '/'}*/}
-                         {/*className="left brand-logo"*/}
-                     {/*>*/}
-                         {/*Emaily*/}
-
-                     {/*</Link>*/}
-                     <a className="left brand-logo">
+                     <Link
+                         to={this.props.auth ? '/surveys' : '/'}
+                         className="left brand-logo">
                          Emaily
-                     </a>
+
+                     </Link>
+                     {/*<a className="left brand-logo">*/}
+                         {/*Emaily*/}
+                     {/*</a>*/}
 
                      <ul className="right">
                          {this.renderContent()}
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,4 @@
+// add these env variables to Heroku setting page
 // prod.js - production keys here!
 // GOOGLE PROJECT questionnaire-prod
 // GOOGLE_CLIENT_ID: 1061640972929-5kugfd4v8obgptq4fih5ecj4ko2o0m9c.apps.googleusercontent.com
@@ -7,5 +8,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey : process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
