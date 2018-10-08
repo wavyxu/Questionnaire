8cf5daa5dac7685bbcb1ac016471e488d60059c1
Wei Xu <wavyxu@gmail.com>
2018-10-07 18:18:04 -0700

    Advanced SurveyForm

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,33 @@
 // SurveyNew show SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {new : true};
+    // }
+
+    state = { showFormReview: false};
+
+    renderContent() {
+        if (this.state.showFormReview === true) {
+            return <SurveyFormReview />;
+        }
+
+        return (
+            <SurveyForm
+                onSurveySubmit={() => this.setState({ showFormReview: true})}
+            />
+        );
+    }
+
     render() {
         return (
             <div>
-                <SurveyForm/>
+                {this.renderContent()}
             </div>
         );
     }
